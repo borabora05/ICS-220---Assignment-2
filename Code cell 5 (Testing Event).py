8e2e51e094ac17627da7c6a134d3af9e683b4f51
borabora05 <125470629+borabora05@users.noreply.github.com>
2024-03-31 05:07:47 +0400

    Create Code cell 5 (Testing Event).py

---
+++ Code cell 5 (Testing Event).py
@@ -0,0 +1,10 @@
+#Testing event.py
+# Create an Event object
+event2 = Event(2, "Concert: Music through the Ages", "2024-05-10", "Auditorium", 20.0)
+
+# Test attribute access
+print(f"Event ID: {event2.id}")
+print(f"Name: {event2.name}")
+print(f"Date: {event2.date}")
+print(f"Location: {event2.location}")
+print(f"Price: {event2.price} AED")
