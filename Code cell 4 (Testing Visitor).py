4d37d4ee241180b251d22a4a38efa86d964b5db5
borabora05 <125470629+borabora05@users.noreply.github.com>
2024-03-31 05:05:45 +0400

    Create Code cell 4 (Testing Visitor).py

---
+++ Code cell 4 (Testing Visitor).py
@@ -0,0 +1,9 @@
+#Testing visitor.py
+# Create a Visitor object
+visitor2 = Visitor(201, "John Doe", 12, "France")
+
+# Test attribute access
+print(f"Visitor ID: {visitor2.id}")
+print(f"Name: {visitor2.name}")
+print(f"Age: {visitor2.age}")
+print(f"Nationality: {visitor2.nationality}")
